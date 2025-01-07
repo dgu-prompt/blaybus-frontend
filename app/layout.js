758a2d21c7912c70a49bdcc6275effa21dfe3d42
Daeyong Jeong <daeyong.jeong.18@icloud.com>
2025-01-07 05:46:18 +0000

    Replace Geist font with local Pretendard font in layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import localFont from 'next/font/local'
+// import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pretendard.className} antialiased`}
       >
         {children}
       </body>
