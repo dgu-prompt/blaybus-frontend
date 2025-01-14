9be9ce73162e09910c3f121a1f47b6e6cb806a4e
Daeyong Jeong <daeyong.jeong.18@icloud.com>
2025-01-15 03:27:25 +0900

    Add getUserInfo function to fetch user data and update page component; improve error handling and user feedback

--- app/(app)/you/page.tsx
+++ app/(app)/you/page.tsx
@@ -2,22 +2,11 @@ import { List, ListItem, ListLink, Section } from "@/components/grouped-list";
 import { NavigationBar } from "@/components/navigation-bar";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { SettingsButton } from "./_components/settings-button";
+import { getUserInfo } from "./_actions/get-user-info";
 
-const userData = {
-  id: "2023010101",
-  name: "김민수",
-  joinDate: "2023-01-01",
-  department: "음성 1센터",
-  jobGroup: "1",
-  level: "F1-Ⅰ",
-  totalExp: 12657,
-  prevYearExp: 5000,
-  currentYearExp: 7657,
-  nextLevelExp: 13500,
-  midAverageExp: 9000, // 올해 획득 가능한 중위평균 경험치
-};
+export default async function Page() {
+  const userData = await getUserInfo();
 
-export default function Page() {
   return (
     <>
       <NavigationBar
@@ -30,24 +19,26 @@ export default function Page() {
           <ListItem>
             <div className="flex w-full flex-col items-center justify-center gap-4 py-6">
               <Avatar className="size-32">
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>KM</AvatarFallback>
+                <AvatarImage src={userData.characterUrl} />
+                <AvatarFallback>{userData.employeeName}</AvatarFallback>
               </Avatar>
-              <span className="text-xl font-semibold">김민수</span>
+              <span className="text-xl font-semibold">
+                {userData.employeeName}
+              </span>
             </div>
           </ListItem>
           <ListLink title="아바타 변경하기" href="#" />
         </Section>
         <Section header="내 정보">
-          <ListItem title="이름" detail={userData.name} />
-          <ListItem title="사번" detail={userData.id} />
+          <ListItem title="이름" detail={userData.employeeName} />
+          <ListItem title="사번" detail={userData.employeeNumber} />
           <ListItem title="입사일" detail={userData.joinDate} />
           <ListItem title="소속" detail={userData.department} />
-          <ListItem title="직무그룹" detail={userData.jobGroup} />
+          <ListItem title="직무그룹" detail={userData.jobGroupId} />
         </Section>
         <Section header="경험치">
           <ListItem title="레벨" detail={userData.level} />
-          <ListItem title="총 경험치" detail={`${userData.totalExp} do`} />
+          {/* <ListItem title="총 경험치" detail={`${userData.totalExp} do`} /> */}
         </Section>
       </List>
     </>
