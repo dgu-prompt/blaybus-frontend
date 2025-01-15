cbaca3719ee97beb65daf5393e4a9f62253dc87d
Daeyong Jeong <daeyong.jeong.18@icloud.com>
2025-01-15 15:05:29 +0900

    Fetch user details from /api/users and display employee name in HelloView component

--- app/(app)/home/_components/hello-view.tsx
+++ app/(app)/home/_components/hello-view.tsx
@@ -4,12 +4,12 @@ import { Today } from "./today";
 // TODO: 유저 아이디 말고 실제 이름을 불러오도록
 export async function getUsername() {
   try {
-    const response = await fetchWithAuth("/api/username");
+    const response = await fetchWithAuth("/api/users");
 
     if (!response.ok) {
       throw new Error("Failed to fetch username.");
     }
-    return await response.text();
+    return await response.json();
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     throw new Error("Failed to fetch username.");
@@ -17,15 +17,14 @@ export async function getUsername() {
 }
 
 export async function HelloView() {
-  const data = await getUsername();
-  const username = data;
+  const user = await getUsername();
 
   return (
     <div className="py-6 text-center font-semibold">
       <div className="text-muted-foreground">
         <Today />
       </div>
-      <div className="text-lg">{username}님, 오늘도 힘내봐요!</div>
+      <div className="text-lg">{user.employeeName}님, 오늘도 힘내봐요!</div>
     </div>
   );
 }
