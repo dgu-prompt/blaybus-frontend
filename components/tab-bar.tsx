e32bed6be73b771c6d1052bb47e79bb833d15b8a
Daeyong Jeong <daeyong.jeong.18@icloud.com>
2025-01-16 02:12:20 +0900

    Refactor TabBar component to use a separate TabBarMenu for dynamic tab rendering based on client-side checks

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { Tab } from "./tab";
 import { Container, Wrapper } from "./container";
 import { Bell, Home, User, Wrench } from "lucide-react";
+import { useIsClient } from "@/hooks/use-is-client";
 
 const tabs = [
   { title: "홈", href: "/home", icon: Home },
@@ -12,29 +13,34 @@ const tabs = [
   { title: "관리자", href: "/admin", icon: Wrench },
 ];
 
-export function TabBar({ isAdmin }: { isAdmin: boolean }) {
+function TabBarMenu({ isAdmin }: { isAdmin: boolean }) {
+  const isClient = useIsClient();
   const pathname = usePathname();
 
   const filteredTabs = isAdmin
     ? tabs
     : tabs.filter((tab) => tab.title !== "관리자");
 
+  if (!isClient) return null;
+
+  return filteredTabs.map((tab, index) => (
+    <Tab
+      key={index}
+      icon={tab.icon}
+      title={tab.title}
+      href={tab.href}
+      active={pathname.startsWith(tab.href)}
+    />
+  ));
+}
+
+export function TabBar({ isAdmin }: { isAdmin: boolean }) {
   return (
     <>
       <div className="grow" />
       <Container className="pb-safe translucent sticky bottom-0 z-50 border-t bg-background/50">
-        <Wrapper className="h-12">
-          <div className="flex h-full w-full items-center">
-            {filteredTabs.map((tab, index) => (
-              <Tab
-                key={index}
-                icon={tab.icon}
-                title={tab.title}
-                href={tab.href}
-                active={pathname.startsWith(tab.href)}
-              />
-            ))}
-          </div>
+        <Wrapper className="h-12 flex-row items-center">
+          <TabBarMenu isAdmin={isAdmin} />
         </Wrapper>
       </Container>
     </>
